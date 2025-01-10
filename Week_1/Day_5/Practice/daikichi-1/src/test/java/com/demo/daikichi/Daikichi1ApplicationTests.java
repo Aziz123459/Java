ccbf22c1ac0463262c893b52811f5f4aa3252d3f
Mohamed Aziz Tounsi <tounsiasiz@gmail.com>
2025-01-10 13:16:37 +0100

    daikichi

---
+++ Week_1/Day_5/Practice/daikichi-1/src/test/java/com/demo/daikichi/Daikichi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.daikichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daikichi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
